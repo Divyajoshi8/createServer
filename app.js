5f89a3edff048396ed217417d9256dd18ccbd06d
Divyajoshi8 <97658852+Divyajoshi8@users.noreply.github.com>
2023-05-17 10:46:46 +0530

    code done

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const http = require('./http');
-function rqListener(){
-    
-}
-http.createServer()
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  console.log('My name is Divya');
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('My name has been logged on the server console.');
+});
+
+server.listen(4000, () => {
+  console.log('Server is listening on port 4000');
+});
